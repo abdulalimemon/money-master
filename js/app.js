18ed65371b47c923855990fce6a099df4e9cc103
Emon02 <abdulalimemon11@gmail.com>
2022-02-17 18:54:39 +0600

    Done

--- js/app.js
+++ js/app.js
@@ -5,11 +5,9 @@ function getMoneyInput(inputId) {
     const parseElementValue = parseFloat(elementValue);
     return parseElementValue;
 }
-
-
-
 // target calculate button
 document.getElementById("calculate").addEventListener("click", function () {
+    // input field
     const income = getMoneyInput("income");
     const food = getMoneyInput("food");
     const rent = getMoneyInput("rent");
@@ -18,38 +16,63 @@ document.getElementById("calculate").addEventListener("click", function () {
     const balance = income - (food + rent + clothes);
     // calculate total expense
     const totalExpense = food + rent + clothes;
-    // target balance and total expense heading
-
+    // error handling
     if (income > 0 && food >= 0 && rent >= 0 && clothes >= 0) {
         if (income > totalExpense) {
+            // target balance and total expense heading
             document.getElementById("balance").innerText = balance;
             document.getElementById("total-expense").innerText = totalExpense;
             document.getElementById("warning").innerText = "";
             document.getElementById("valid-income").innerText = "";
+            document.getElementById("saving-amount").innerText = "";
+            document.getElementById("remaining-balance").innerText = "";
         } else {
             document.getElementById("warning").innerText = "You can't spend more than what you earn";
             document.getElementById("balance").innerText = "";
             document.getElementById("total-expense").innerText = "";
             document.getElementById("valid-income").innerText = "";
+            document.getElementById("saving-amount").innerText = "";
+            document.getElementById("remaining-balance").innerText = "";
         }
-    }else {
+    } else {
         document.getElementById("warning").innerText = "";
         document.getElementById("balance").innerText = "";
         document.getElementById("total-expense").innerText = "";
+        document.getElementById("saving-amount").innerText = "";
+        document.getElementById("remaining-balance").innerText = "";
         document.getElementById("valid-income").innerText = "Input Right amount";
     }
-
-
+})
+// target save button
+document.getElementById("save-btn").addEventListener("click", function () {
+    // input field
+    const income = getMoneyInput("income");
+    const food = getMoneyInput("food");
+    const rent = getMoneyInput("rent");
+    const clothes = getMoneyInput("clothes");
+    const save = getMoneyInput("save-input");
+    // calculate balance
+    const balance = income - (food + rent + clothes);
+    // calculate saving amount
+    const savingAmount = (income / 100) * save;
+    // calculate remaining Balance
+    const remainingBalance = balance - savingAmount;
+    // error handling
+    if (save <= 100) {
+        if (balance < savingAmount) {
+            document.getElementById("valid-amount").innerText = "You cannot save more than your available balance";
+            document.getElementById("saving-amount").innerText = "";
+            document.getElementById("remaining-balance").innerText = "";
+        } else {
+            document.getElementById("saving-amount").innerText = savingAmount;
+            document.getElementById("remaining-balance").innerText = remainingBalance;
+            document.getElementById("valid-amount").innerText = "";
+        }
+    } else {
+        document.getElementById("valid-amount").innerText = "You cannot save more than 100%";
+        document.getElementById("saving-amount").innerText = "";
+        document.getElementById("remaining-balance").innerText = "";
+    }
 
 })
-
-// if (food >= 0) {
-   
-// } else {
-//     document.getElementById("valid-amount").innerText = "Input Right amount";
-//     document.getElementById("total-expense").innerText = "";
-//     document.getElementById("balance").innerText = "";
-//     document.getElementById("warning").innerText = "";
-// }
-
 
