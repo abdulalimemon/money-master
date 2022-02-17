a413357bc451ab6342e43883b3e51ed86aa9b22c
Emon02 <abdulalimemon11@gmail.com>
2022-02-17 16:58:59 +0600

    error check

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Get Income, food, Rent, Clothes Input Value
 function getMoneyInput(inputId) {
     const elementID = document.getElementById(inputId);
     const elementValue = elementID.value;
@@ -7,19 +8,45 @@ function getMoneyInput(inputId) {
 
 
 
-
+// target calculate button
 document.getElementById("calculate").addEventListener("click", function () {
     const income = getMoneyInput("income");
     const food = getMoneyInput("food");
     const rent = getMoneyInput("rent");
     const clothes = getMoneyInput("clothes");
-
-    const balance = income - ( food + rent + clothes);
-
+    // calculate balance
+    const balance = income - (food + rent + clothes);
+    // calculate total expense
     const totalExpense = food + rent + clothes;
+    // target balance and total expense heading
 
-    document.getElementById("balance").innerText = balance;
-    document.getElementById("total-expense").innerText = totalExpense;
+    if (income > 0) {
+        if (income > totalExpense) {
+            if (food >= 0 || typeof food != "number") {
+                document.getElementById("balance").innerText = balance;
+                document.getElementById("total-expense").innerText = totalExpense;
+                document.getElementById("warning").innerText = "";
+                document.getElementById("valid-income").innerText = "";
+            } else {
+                document.getElementById("valid-amount").innerText = "Input Right amount";
+                document.getElementById("warning").innerText = "";
+                document.getElementById("balance").innerText = "";
+                document.getElementById("warning").innerText = "";
+            }
+        } else {
+            document.getElementById("warning").innerText = "You can't spend more than what you earn";
+            document.getElementById("balance").innerText = "";
+            document.getElementById("total-expense").innerText = "";
+            document.getElementById("valid-income").innerText = "";
+        }
+    } else {
+        document.getElementById("warning").innerText = "";
+        document.getElementById("balance").innerText = "";
+        document.getElementById("total-expense").innerText = "";
+        document.getElementById("valid-income").innerText = "Input Right amount";
+    }
+
+
 
 })
 
